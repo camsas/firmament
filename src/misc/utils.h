dd4daa3b87805436aba477ef6ec8ab651eb515f9
Malte Schwarzkopf <malte.schwarzkopf@cl.cam.ac.uk>
2012-01-18 13:40:40 +0000

    [misc] Code cleanup, headers and comments.

--- src/misc/utils.h
+++ src/misc/utils.h
@@ -1,4 +1,7 @@
-// TODO: header
+// The Firmament project
+// Copyright (c) 2011-2012 Malte Schwarzkopf <malte.schwarzkopf@cl.cam.ac.uk>
+//
+// Miscellaneous utility functions. Descriptions with their declarations.
 
 #ifndef FIRMAMENT_MISC_UTILS_H
 #define FIRMAMENT_MISC_UTILS_H
@@ -8,7 +11,14 @@
 
 namespace firmament {
 
+// Computes a UID for a job as a uint64 by hashing the job name.
+// TODO(malte): This may become deprecated in the future, as the job descriptor
+//              may carry this information.
 uint64_t MakeJobUID(Job *job);
+
+// Computes a UID for an ensemble as a uint64 by hashing the job name.
+// TODO(malte): This may become deprecated in the future, as the ensemble
+//              descriptor may carry this information.
 uint64_t MakeEnsembleUID(Ensemble *ens);
 
 }  // namespace firmament
