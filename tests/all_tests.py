1820fba1687b3487afe5b53dd2496f1ce71670f2
Malte Schwarzkopf <malte.schwarzkopf@cl.cam.ac.uk>
2012-01-30 15:32:37 +0000

    [tests] Improved/fixed unit test infrastructure.

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -11,8 +11,23 @@ try:
     print "* RUNNING: %s" % line.strip()
     print "******************************************************************" \
         "*************"
-    subprocess.call([line, "--logtostderr"], shell=True)
+    # run the next unit test
+    subprocess.check_call([line.strip(), "--logtostderr"],
+                          stdout=sys.stdout, stderr=sys.stderr)
+
+  # we actually made it to the end
+  print "====================================================================" \
+      "===========\n" \
+      "ALL UNIT TESTS PASSING :-)"
+except subprocess.CalledProcessError as e:
+  print "====================================================================" \
+      "===========\n" \
+      "UNIT TEST(s) FAILED :-( See above for details. \n" \
+      "====================================================================" \
+      "===========\n"
 except Exception as e:
-  print "Failed to run all tests. Does all_tests.txt in $BUILD_DIR/tests/" \
-        "exist?"
+  print "Failed to run all tests. Check the following: \n" \
+        "1) Does all_tests.txt in $BUILD_DIR/tests/ exist?" \
+        "2) Do all test binaries listed in all_tests.txt exist and are they " \
+        "executable?"
   print e
