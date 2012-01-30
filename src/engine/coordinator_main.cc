b63c1cc93564a6e3e99c3d958d33e1156e959850
Malte Schwarzkopf <malte.schwarzkopf@cl.cam.ac.uk>
2012-01-30 15:31:58 +0000

    [engine] Adding main wrapper for coordinator.

---
+++ src/engine/coordinator_main.cc
@@ -0,0 +1,36 @@
+// The Firmament project
+// Copyright (c) 2011-2012 Malte Schwarzkopf <malte.schwarzkopf@cl.cam.ac.uk>
+//
+// Initialization code for coordinator binary. This delegates to the
+// Coordinator class almost immediately after launching.
+
+#include <stdint.h>
+#include <iostream>
+
+#include "base/common.h"
+#include "engine/coordinator.h"
+#include "platforms/common.h"
+
+#include "platforms/common.pb.h"
+
+using namespace firmament;
+
+// --platform argument: string matching members of the <PlatformID> enum
+DECLARE_string(platform);
+
+// The main method: initializes, parses arguments and sets up a worker for
+// the platform we're running on.
+int main(int argc, char *argv[]) {
+  VLOG(1) << "Calling common::InitFirmament";
+  common::InitFirmament(argc, argv);
+
+  // TODO(malte): support for automatic platform detection?
+  // TODO(malte): validation of FLAGS_platform
+  PlatformID platform_id = GetPlatformID(FLAGS_platform);
+
+  LOG(INFO) << "Firmament coordinator starting (Platform: " << platform_id
+            << ") ...";
+  Coordinator coordinator(platform_id);
+
+  coordinator.Run();
+}
