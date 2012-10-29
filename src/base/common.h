e213a996543be0d8d4bf87f8850d9e99ec2858c5
ms705 <ms705-git@srcf.ucam.org>
2012-10-29 10:18:38 +0000

    [base] Helper functions to convert repeated PB fields to STL data structures.

--- src/base/common.h
+++ src/base/common.h
@@ -18,10 +18,13 @@
 #include <gflags/gflags.h>
 #include <gtest/gtest_prod.h>
 #include <google/protobuf/message.h>
+#include <google/protobuf/repeated_field.h>
+
+namespace firmament {
 
 using namespace std;  // NOLINT
 
-namespace firmament {
+using google::protobuf::RepeatedPtrField;
 
 #define SUBMSG_READ(obj, submsg, member) \
     obj.GetExtension(submsg ## _extn).member()
@@ -46,6 +49,32 @@ inline void InitFirmament(int argc, char *argv[]) {
 
   // Set up glog for logging output
   google::InitGoogleLogging(argv[0]);
+}
+
+// Helper function to convert a repeated protobuf field to a STL set.
+// This method copies the input collection, so it is O(N) in time and space.
+template <typename T>
+inline set<T> pb_to_set(const RepeatedPtrField<T>& pb_field) {
+  set<T> return_set;
+  // N.B.: using GNU-style RTTI (typeof)
+  for (typeof(pb_field.begin()) iter = pb_field.begin();
+       iter != pb_field.end();
+       ++iter)
+    return_set.insert(*iter);
+  return return_set;
+}
+
+// Helper function to convert a repeated protobuf field to a STL vector.
+// This method copies the input collection, so it is O(N) in time and space.
+template <typename T>
+inline vector<T> pb_to_vector(const RepeatedPtrField<T>& pb_field) {
+  vector<T> return_vec;
+  // N.B.: using GNU-style RTTI (typeof)
+  for (typeof(pb_field.begin()) iter = pb_field.begin();
+       iter != pb_field.end();
+       ++iter)
+    return_vec.push_back(*iter);
+  return return_vec;
 }
 
 }  // namespace firmament::common
