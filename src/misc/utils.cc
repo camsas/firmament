07b2ab0cc20496f139fd0312fbb805dd8cef5c03
ms705 <ms705-git@srcf.ucam.org>
2012-10-29 17:51:00 +0000

    [misc] Move some utility functions into general utility file.

--- src/misc/utils.cc
+++ src/misc/utils.cc
@@ -5,11 +5,11 @@
 
 #include <boost/functional/hash.hpp>
 
-#include "misc/utils.h"
-
 #include <string>
 // N.B.: C header for gettimeofday()
 #include <sys/time.h>
+
+#include "misc/utils.h"
 
 namespace firmament {
 
@@ -19,7 +19,7 @@ uint64_t GetCurrentTimestamp() {
   return ts.tv_sec * 1000000 + ts.tv_usec;
 }
 
-uint64_t MakeJobUID(Job *job) {
+/*uint64_t MakeJobUID(Job *job) {
   CHECK_NOTNULL(job);
   boost::hash<string> hasher;
   return hasher(job->name());
@@ -30,6 +30,28 @@ uint64_t MakeEnsembleUID(Ensemble *ens) {
   VLOG(1) << ens->name();
   boost::hash<string> hasher;
   return hasher(ens->name());
+}*/
+
+ResourceID_t GenerateUUID() {
+  boost::uuids::random_generator gen;
+  return gen();
+}
+
+JobID_t GenerateJobID() {
+  boost::uuids::random_generator gen;
+  return gen();
+}
+
+JobID_t JobIDFromString(const string& str) {
+  // XXX(malte): This makes assumptions about JobID_t being a Boost UUID. We
+  // should have a generic "JobID_t-from-string" helper instead.
+#ifdef __PLATFORM_HAS_BOOST__
+  boost::uuids::string_generator gen;
+  boost::uuids::uuid job_uuid = gen(str);
+#else
+  string job_uuid = str;
+#endif
+  return job_uuid;
 }
 
 }  // namespace firmament
