21efe35bfb3531a056501ecdac46ad9b11b7a244
ms705 <ms705-git@srcf.ucam.org>
2012-11-16 14:16:02 +0000

    [engine] Add an abstract node class to centralized communication functionality shared between worker and coordinator (and possibly TaskLib, too.)

---
+++ src/engine/node.cc
@@ -0,0 +1,170 @@
+// The Firmament project
+// Copyright (c) 2011-2012 Malte Schwarzkopf <malte.schwarzkopf@cl.cam.ac.uk>
+//
+// Implementation of some common utility methods for Firmament nodes. Note that
+// Node is an abstract class, so it cannot be instantiated directly.
+
+#include "engine/node.h"
+
+#include <string>
+#include <utility>
+
+#ifdef __PLATFORM_HAS_BOOST__
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#endif
+
+#include "base/resource_desc.pb.h"
+#include "messages/base_message.pb.h"
+#include "misc/protobuf_envelope.h"
+#include "misc/map-util.h"
+#include "misc/utils.h"
+
+DEFINE_string(platform, "AUTO", "The platform we are running on, or AUTO for "
+              "attempting automatic discovery.");
+DEFINE_string(listen_uri, "tcp://localhost:9998",
+              "The name/address/port to listen on.");
+
+namespace firmament {
+
+// Initial value of exit_ toggle
+bool Node::exit_ = false;
+
+Node::Node(PlatformID platform_id, ResourceID_t uuid)
+  : platform_id_(platform_id),
+    uuid_(uuid) {
+  // Set up the node's resource descriptor
+  resource_desc_.set_uuid(to_string(uuid_));
+  resource_desc_.set_type(ResourceDescriptor::RESOURCE_MACHINE);
+
+  switch (platform_id) {
+    case PL_UNIX: {
+      // Set up a message adapter for control messages.
+      m_adapter_.reset(
+          new platform_unix::streamsockets::
+          StreamSocketsAdapter<BaseMessage>());
+      // Also set up a signal handler so that we can quit the node using
+      // signals.
+      SignalHandler handler;
+      handler.ConfigureSignal(SIGINT, Node::HandleSignal, this);
+      handler.ConfigureSignal(SIGTERM, Node::HandleSignal, this);
+      break;
+    }
+    default:
+      LOG(FATAL) << "Unimplemented!";
+  }
+}
+
+Node::~Node() {
+}
+
+bool Node::ConnectToRemote(
+    const string& remote_uri,
+    shared_ptr<StreamSocketsChannel<BaseMessage> > chan) {
+  return m_adapter_->EstablishChannel(remote_uri, chan);
+}
+
+bool Node::SendMessageToRemote(
+    shared_ptr<StreamSocketsChannel<BaseMessage> > chan,
+    BaseMessage* msg) {
+  Envelope<BaseMessage> envelope(msg);
+  return chan->SendA(
+      envelope, boost::bind(&Node::HandleWrite,
+                            this,
+                            boost::asio::placeholders::error,
+                            boost::asio::placeholders::bytes_transferred));
+}
+
+void Node::HandleWrite(const boost::system::error_code& error,
+                              size_t bytes_transferred) {
+  VLOG(2) << "In HandleWrite, thread is " << boost::this_thread::get_id();
+  if (error)
+    LOG(ERROR) << "Error returned from async write: " << error.message();
+  else
+    VLOG(1) << "bytes_transferred: " << bytes_transferred;
+}
+
+void Node::Run() {
+  // Node starting -- set up and wait for others to connect.
+  m_adapter_->Listen(FLAGS_listen_uri);
+  m_adapter_->RegisterAsyncMessageReceiptCallback(
+      boost::bind(&Node::HandleIncomingMessage, this, _1));
+  m_adapter_->RegisterAsyncErrorPathCallback(
+      boost::bind(&Node::HandleIncomingReceiveError, this,
+                  boost::asio::placeholders::error, _2));
+
+  // Main loop
+  while (!exit_) {
+    // Wait for events (i.e. messages from workers.
+    // TODO(malte): we need to think about any actions that the coordinator
+    // itself might need to take, and how they can be triggered
+    VLOG(3) << "Hello from main loop!";
+    AwaitNextMessage();
+  }
+
+  // We have dropped out of the main loop and are exiting
+  // TODO(malte): any cleanup we need to do; hand-over to another coordinator if
+  // possible?
+  Shutdown("dropped out of main loop");
+}
+
+void Node::AwaitNextMessage() {
+  VLOG(3) << "Waiting for next message from adapter...";
+  m_adapter_->AwaitNextMessage();
+  boost::this_thread::sleep(boost::posix_time::seconds(1));
+}
+
+void Node::HandleRecv(const boost::system::error_code& error,
+                             size_t bytes_transferred,
+                             Envelope<BaseMessage>* env) {
+  if (error) {
+    LOG(WARNING) << "Asynchronous receive call returned an error: "
+                 << error.message();
+    return;
+  }
+  VLOG(3) << "Received " << bytes_transferred << " bytes asynchronously, "
+          << "in envelope at " << env << ", representing message " << *env;
+  BaseMessage *bm = env->data();
+  HandleIncomingMessage(bm);
+  delete env;
+}
+
+void Node::HandleIncomingReceiveError(
+    const boost::system::error_code& error,
+    const string& remote_endpoint) {
+  // Notify of receive error
+  // TODO(malte): since we are taking no arguments, we actually don't have the
+  // faintest idea what message this error relates to. We should try to remedy
+  // this; however, it is not trivial, since we destroy the channel before
+  // making the callback (and we do not want to have the callback on the
+  // critical path to unlocking the receive lock, as it may take a long time to
+  // run).
+  if (error.value() == boost::asio::error::eof) {
+    // Connection terminated, handle accordingly
+    LOG(INFO) << "Connection to " << remote_endpoint << " closed.";
+    // XXX(malte): Need to figure out if this relates to a resource, and if so,
+    // if we should declare it failed; or whether this is an expected job
+    // completion.
+  } else {
+    LOG(WARNING) << "Failed to complete a message receive cycle from "
+                 << remote_endpoint << ". The message was discarded, or the "
+                 << "connection failed (error: " << error.message() << ", "
+                 << "code " << error.value() << ").";
+  }
+}
+
+void Node::HandleSignal(int signum) {
+  // TODO(malte): stub
+  if (signum == SIGTERM || signum == SIGINT)
+    exit_ = true;
+}
+
+void Node::Shutdown(const string& reason) {
+  LOG(INFO) << "Node shutting down; reason: " << reason;
+  m_adapter_->StopListen();
+  VLOG(1) << "All connections shut down; now exiting...";
+  // Toggling the exit flag will make the Node drop out of its main loop.
+  exit_ = true;
+}
+
+}  // namespace firmament
---
+++ src/engine/node.h
@@ -0,0 +1,93 @@
+// The Firmament project
+// Copyright (c) 2011-2012 Malte Schwarzkopf <malte.schwarzkopf@cl.cam.ac.uk>
+//
+// General node class. This provides implementations of shared utility methods,
+// such as those used for connecting to and communicating with remote nodes.
+
+#ifndef FIRMAMENT_ENGINE_NODE_H
+#define FIRMAMENT_ENGINE_NODE_H
+
+#include <string>
+
+// XXX(malte): Think about the Boost dependency!
+#ifdef __PLATFORM_HAS_BOOST__
+#include <boost/thread.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/uuid/uuid.hpp>
+#else
+// Currently this won't build if __PLATFORM_HAS_BOOST__ is not defined.
+#error __PLATFORM_HAS_BOOST__ not set, so cannot build node!
+#endif
+
+#include "base/common.h"
+#include "base/types.h"
+#include "base/resource_desc.pb.h"
+// XXX(malte): include order dependency
+#include "platforms/unix/common.h"
+#include "platforms/unix/signal_handler.h"
+#include "misc/messaging_interface.h"
+#include "platforms/common.h"
+#include "platforms/unix/stream_sockets_adapter.h"
+#include "platforms/unix/stream_sockets_adapter-inl.h"
+
+namespace firmament {
+
+using platform_unix::SignalHandler;
+using platform_unix::streamsockets::StreamSocketsChannel;
+using platform_unix::streamsockets::StreamSocketsAdapter;
+
+class Node : public boost::enable_shared_from_this<Node> {
+ public:
+  Node(PlatformID platform_id, ResourceID_t uuid);
+  virtual ~Node();
+  virtual void Run() = 0;
+  void Shutdown(const string& reason);
+
+  inline PlatformID platform_id() {
+    return platform_id_;
+  }
+  inline ResourceID_t uuid() { return uuid_; }
+
+ protected:
+  void AwaitNextMessage();
+  bool ConnectToRemote(const string& remote_uri,
+                       shared_ptr<StreamSocketsChannel<BaseMessage> > chan);
+  bool SendMessageToRemote(
+      shared_ptr<StreamSocketsChannel<BaseMessage> > chan,
+      BaseMessage* msg);
+  virtual void HandleIncomingMessage(BaseMessage *bm) = 0;
+  void HandleIncomingReceiveError(const boost::system::error_code& error,
+                                  const string& remote_endpoint);
+  void HandleRecv(const boost::system::error_code& error,
+                  size_t bytes_transferred,
+                  Envelope<BaseMessage>* env);
+  void HandleWrite(const boost::system::error_code& error,
+                   size_t bytes_transferred);
+#if (BOOST_VERSION < 104700)
+  // compatible with C-style signal handler setup
+  static void HandleSignal(int signum);
+#else
+  // Boost ASIO signal handler setup
+  static void HandleSignal(int signum);
+#endif
+
+  // The node's platform's ID.
+  // TODO(malte): This is deprecated and may be removed in the future.
+  PlatformID platform_id_;
+  // Boolean flag that when set to true will cause the node to shut down.
+  static bool exit_;
+  // The configured URI at which this node is reachable.
+  string node_uri_;
+  // A messaging adapter for the exchange of control messages.
+  // TODO(malte): This is specialized to BaseMessage protobufs here. Maybe it
+  // should not be?
+  shared_ptr<StreamSocketsAdapter<BaseMessage> > m_adapter_;
+  // This node's own resource descriptor.
+  ResourceDescriptor resource_desc_;
+  // The node's resource UUID.
+  ResourceID_t uuid_;
+};
+
+}  // namespace firmament
+
+#endif  // FIRMAMENT_ENGINE_NODE_H
