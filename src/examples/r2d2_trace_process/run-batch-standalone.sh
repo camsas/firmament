8b6e6ca223d39739c264a2ba0092cc3c0a02fa1d
ms705 <ms705-git@srcf.ucam.org>
2013-01-23 14:27:28 +0000

    [example] Script to run R2D2 analyses standalone.

---
+++ src/examples/r2d2_trace_process/run-batch-standalone.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+DIR=/mnt/datasets
+COUNT=0
+OUT_DIR=/mnt/datasets/results
+PATTERN=$1
+
+#for d in `cd ${DIR}; ls -d *${PATTERN}*/`; do
+for d in `cd ${DIR}; ls *${PATTERN}*.tar.bz2`; do
+  f=`echo ${d} | sed -s 's/.tar.bz2//'`
+  if [[ ! -d ${DIR}/${f} ]]; then
+    cd ${DIR}
+    ./extract.sh ${f}
+    cd -
+  else
+    echo "No extraction required."
+  fi
+#  mkdir -p ${OUT_DIR}/$d
+  echo "PACKET JOIN"
+  ./packet_join_task ${DIR}/${f}/dag_cap_0 ${DIR}/${f}/dag_cap_1 0 ${COUNT} --v=1 --logtostderr 1>${OUT_DIR}/${f}/results.csv
+  echo "TRACE 0 ANALYSIS"
+  ./simple_trace_analysis_task ${DIR}/${f}/dag_cap_0 0 ${COUNT} --v=1 --logtostderr 1>${OUT_DIR}/${f}/results-dag0.csv
+  echo "TRACE 1 ANALYSIS"
+  ./simple_trace_analysis_task ${DIR}/${f}/dag_cap_1 0 ${COUNT} --v=1 --logtostderr 1>${OUT_DIR}/${f}/results-dag1.csv
+  echo "TRACE 0 BANDWIDTH"
+  ./aggregate_bandwidth_analysis_task ${DIR}/${f}/dag_cap_0 0 ${COUNT} --v=1 --logtostderr 1>${OUT_DIR}/${f}/results-bw-dag0.csv
+  echo "TRACE 1 BANDWIDTH"
+  ./aggregate_bandwidth_analysis_task ${DIR}/${f}/dag_cap_1 0 ${COUNT} --v=1 --logtostderr 1>${OUT_DIR}/${f}/results-bw-dag1.csv
+done
