b21abb5ad40b59d1a53e628eba5a19a34f1920ff
ms705 <ms705-git@srcf.ucam.org>
2013-02-19 17:53:47 +0000

    [examples] More extensive store test; temporarily revert to cout instead of log usage.

--- src/examples/store_example/store_example.cc
+++ src/examples/store_example/store_example.cc
@@ -27,21 +27,29 @@ namespace store {
 void StoreTask::Invoke() {
   // a = read input 1
   // b = read input 2
-  LOG(INFO) << "Cache is " << task_lib_->getCache()->size;
+  cout << "Cache is " << task_lib_->getCache()->size << endl;
   // cout << "Testing Get Object Start when no object present ";
   // void* read_ptr = task_lib_->GetObjectStart(0);
   // if (read_ptr==NULL) cout <<" Ptr is null ";
 
-  LOG(INFO) << "Testing Put Object Start ";
+  cout << "Testing Put Object Start " << endl;
   void* write_ptr = task_lib_->PutObjectStart(1234, 10);
   std::memset(write_ptr, 1, 3);
-  LOG(INFO) << "Testing Put Object End ";
+  cout << " Write ptr is " << write_ptr << endl;
+  cout << " Written value: " << hex
+       << *static_cast<uint64_t*>(write_ptr) << endl;
+  cout << "Testing Put Object End " << endl;
   task_lib_->PutObjectEnd(1234, 10);
-  LOG(INFO) << "Testing Get Object Start when  object present ";
+  cout << "Testing Get Object Start when  object present " << endl;
   void* read_ptr2 = task_lib_->GetObjectStart(1234);
-  if (read_ptr2 == NULL)
-    LOG(INFO) <<" Ptr is null ";
-  LOG(INFO) << "Testing Get Object End when object present ";
+  if (read_ptr2 == NULL) {
+    cout << " Ptr is null " << endl;
+  } else {
+    cout << " Ptr is " << read_ptr2 << endl;
+    cout << " Value read: " << hex
+         << *static_cast<uint64_t*>(read_ptr2) << endl;
+  }
+  cout << "Testing Get Object End when object present " << endl;
   task_lib_->GetObjectEnd(1234);
 }
 
