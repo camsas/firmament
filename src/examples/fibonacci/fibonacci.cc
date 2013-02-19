d21ed0dbc4b0ec3f509cefd489e8ac5647bd01f8
ms705 <ms705-git@srcf.ucam.org>
2013-02-19 15:30:51 +0000

    [examples] Somewhat hacky fibonacci example with new task spawn API; not yet working.

--- src/examples/fibonacci/fibonacci.cc
+++ src/examples/fibonacci/fibonacci.cc
@@ -28,17 +28,31 @@ namespace fibonacci {
 void FibonacciTask::Invoke(uint64_t n) {
   // a = read input 1
   // b = read input 2
+  uint64_t c;
   if (n <= 1) {
-    uint64_t c = n;
+    c = n;
     VLOG(1) << "F_" << n << " is " << c;
   } else {
     ConcreteReference r(0);
     // TODO(malte): args!
-    vector<FutureReference>* o = NULL;
-    task_lib_->Spawn(r, o); // f(n-1)
-    task_lib_->Spawn(r, o); // f(n-2)
+    FutureReference f_tmpl(0);  // XXX(malte): hack!
+    vector<FutureReference> o1(1, f_tmpl);
+    vector<FutureReference> o2(1, f_tmpl);
+    task_lib_->Spawn(r, NULL, &o1); // f(n-1)
+    task_lib_->Spawn(r, NULL, &o2); // f(n-2)
+    uint64_t* r0_ptr = static_cast<uint64_t*>(
+        task_lib_->GetObjectStart(o1[0].id()));
+    uint64_t* r1_ptr = static_cast<uint64_t*>(
+        task_lib_->GetObjectStart(o2[0].id()));
+    task_lib_->GetObjectEnd(o1[0].id());
+    task_lib_->GetObjectEnd(o2[0].id());
+    // c = f(n-1) + f(n-2)
+    c = *r0_ptr + *r1_ptr;
   }
   // write c to output 1
+  // PutObjectStart();
+  // PutObjectEnd();
+  // Publish(out);
 }
 
 }  // namespace fibonacci
