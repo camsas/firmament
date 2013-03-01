5b848c2206cf23174592789bc2cf961550c6dd5d
ms705 <ms705-git@srcf.ucam.org>
2013-03-01 11:53:14 +0000

    [engine] Minor fix to includ storage engine interface information in resource descriptor.

--- src/engine/coordinator.cc
+++ src/engine/coordinator.cc
@@ -56,6 +56,7 @@ Coordinator::Coordinator(PlatformID platform_id)
   resource_desc_.set_uuid(to_string(uuid_));
   resource_desc_.set_friendly_name(desc_name);
   resource_desc_.set_type(ResourceDescriptor::RESOURCE_MACHINE);
+  resource_desc_.set_storage_engine(object_store_->get_listening_interface());
   local_resource_topology_->mutable_resource_desc()->CopyFrom(
       resource_desc_);
   // Log information
