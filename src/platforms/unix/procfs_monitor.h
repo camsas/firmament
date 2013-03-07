9f4a4e003a94447852f8002247fbc503d275cd56
ms705 <ms705-git@srcf.ucam.org>
2013-03-07 14:47:08 +0000

    [unix] Add ProcFSMonitor for runtime task performance monitoring.

---
+++ src/platforms/unix/procfs_monitor.h
@@ -0,0 +1,113 @@
+// The Firmament project
+// Copyright (c) 2011-2012 Malte Schwarzkopf <malte.schwarzkopf@cl.cam.ac.uk>
+//
+// Simple wrapper to poll information from ProcFS.
+
+#ifndef FIRMAMENT_PLATFORMS_UNIX_PROCFS_MONITOR_H
+#define FIRMAMENT_PLATFORMS_UNIX_PROCFS_MONITOR_H
+
+#include "platforms/unix/common.h"
+
+#include <stdio.h>
+
+#include <string>
+#include <vector>
+
+#include <boost/thread/condition.hpp>
+
+namespace firmament {
+namespace platform_unix {
+
+struct ProcessStatistics {
+  uint64_t pid;
+  char comm[PATH_MAX];
+  char state;
+
+  uint64_t ppid;
+  uint64_t pgid;
+  uint64_t sid;
+  uint64_t tty_nr;
+  uint64_t tpgid;
+
+  uint64_t flags;
+  uint64_t minflt;
+  uint64_t cminflt;
+  uint64_t majflt;
+  uint64_t cmajflt;
+  uint64_t utime;
+  uint64_t stime;
+
+  uint64_t cutime;
+  uint64_t cstime;
+  uint64_t priority;
+  uint64_t nice;
+  uint64_t num_threads;
+
+  uint64_t starttime;
+
+  uint64_t vsize;
+  uint64_t rss;
+  uint64_t rsslim;
+  uint64_t startcode;
+  uint64_t endcode;
+  uint64_t startstack;
+  uint64_t esp;
+  uint64_t eip;
+
+  uint64_t pending;
+  uint64_t blocked;
+  uint64_t sigign;
+  uint64_t sigcatch;
+  uint64_t wchan;
+  uint64_t zero1;
+  uint64_t zero2;
+  uint64_t exit_signal;
+  uint64_t cpu;
+  uint64_t rt_priority;
+  uint64_t policy;
+};
+
+struct SystemStatistics {
+  uint64_t total_memory_;
+  uint64_t free_memory_;
+};
+
+class ProcFSMonitor {
+ public:
+  typedef ProcessStatistics ProcessStatistics_t;
+  typedef SystemStatistics SystemStatistics_t;
+  explicit ProcFSMonitor(uint64_t polling_frequency);
+  const ProcessStatistics_t* ProcessInformation(pid_t pid,
+      ProcessStatistics_t* stats);
+  void Run();
+  void RunForPID(pid_t pid);
+  void Stop();
+  const SystemStatistics_t SystemInformation();
+
+  inline uint64_t ticks_per_sec() { return ticks_per_sec_; }
+  inline uint32_t page_size() { return page_size_; }
+
+ protected:
+  boost::condition stop_;
+  boost::mutex stop_mut_;
+
+ private:
+  // The polling frequency, specified in microseconds
+  uint64_t polling_frequency_;
+  uint64_t ticks_per_sec_;
+  uint32_t page_size_;
+  // Find a line matching the regular expression provided
+  vector<string>* FindMatchingLine(const string& regexp, const string& data);
+  void ProcessPIDStat(pid_t pid, ProcessStatistics_t* stats);
+  inline void readone(FILE* input, uint64_t *x) { fscanf(input, "%lld ", x); }
+  inline void readunsigned(FILE* input, uint64_t *x) {
+    fscanf(input, "%llu ", x);
+  }
+  inline void readstr(FILE* input, char *x) {  fscanf(input, "%s ", x);}
+  inline void readchar(FILE* input, char *x) {  fscanf(input, "%c ", x);}
+};
+
+}  // namespace platform_unix
+}  // namespace firmament
+
+#endif  // FIRMAMENT_PLATFORMS_UNIX_PROCFS_MONITOR_H
---
+++ src/platforms/unix/procfs_monitor_test.cc
@@ -0,0 +1,80 @@
+// The Firmament project
+// Copyright (c) 2011-2012 Malte Schwarzkopf <malte.schwarzkopf@cl.cam.ac.uk>
+//
+// ProcFS monitor unit tests.
+
+#include <gtest/gtest.h>
+
+#include <boost/thread.hpp>
+
+#include "base/common.h"
+#include "platforms/common.pb.h"
+#include "platforms/unix/common.h"
+#include "platforms/unix/procfs_monitor.h"
+
+using firmament::common::InitFirmament;
+
+namespace firmament {
+namespace platform_unix {
+
+// The fixture for testing the stream socket messaging adapter.
+class ProcFSMonitorTest : public ::testing::Test {
+ protected:
+  // You can remove any or all of the following functions if its body
+  // is empty.
+
+  ProcFSMonitorTest()
+    : pfsm_(100000UL)  { // 0.1s polling freq
+    // You can do set-up work for each test here.
+  }
+
+  virtual ~ProcFSMonitorTest() {
+    // You can do clean-up work that doesn't throw exceptions here.
+  }
+
+  // If the constructor and destructor are not enough for setting up
+  // and cleaning up each test, you can define the following methods:
+
+  virtual void SetUp() {
+    // Code here will be called immediately after the constructor (right
+    // before each test).
+  }
+
+  virtual void TearDown() {
+    // Code here will be called immediately after each test (right
+    // before the destructor).
+  }
+
+  // Objects declared here can be used by all tests.
+  ProcFSMonitor pfsm_;
+};
+
+// Tests retrieval of simple process statistics.
+TEST_F(ProcFSMonitorTest, SimpleProcessStatsTest) {
+  pid_t pid = getpid();
+  const ProcFSMonitor::ProcessStatistics_t* stats =
+      pfsm_.ProcessInformation(pid, NULL);
+  CHECK_EQ(stats->pid, pid);
+  malloc(getpagesize());
+  // Should have more pags allocated now...
+  CHECK_LT(stats->rss, pfsm_.ProcessInformation(pid, NULL)->rss);
+}
+
+// Tests retrieval of simple process statistics.
+TEST_F(ProcFSMonitorTest, SimpleSysInfoTest) {
+  FLAGS_v = 2;
+  boost::thread t(&ProcFSMonitor::Run, &pfsm_);
+  sleep(1);
+  pfsm_.Stop();
+  t.join();
+}
+
+
+}  // namespace platform_unix
+}  // namespace firmament
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  InitFirmament(argc, argv);
+  return RUN_ALL_TESTS();
+}
