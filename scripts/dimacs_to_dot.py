b3b12fada2962a7ffe716108e411f4e519db761c
ms705 <ms705-git@srcf.ucam.org>
2013-03-11 19:13:25 +0000

    [scripts] Add capacities and costs to edges in dot output.

--- scripts/dimacs_to_dot.py
+++ scripts/dimacs_to_dot.py
@@ -22,7 +22,9 @@ def dot_out(nodes, edges):
   print "\t{ edge [color=\"#ff0000\"]"
   print "\t",
   for e in edges:
-    print "%d -> %d" % (e['src'], e['dst']),
+    print "%d -> %d [ label = \"%d/%d/%d\" ];" % (e['src'], e['dst'],
+                                                  e['cap_lb'], e['cap_ub'],
+                                                  e['cost'])
   print "\t}"
   # dot footer
   print "}"
@@ -40,14 +42,15 @@ for line in open(inputfile).readlines():
   if fields[0] == 'n':
     # node
     node = { 'nid': int(fields[1]),
-             'supp': float(fields[2]) }
+             'supp': int(fields[2]) }
     nodes.append(node)
   if fields[0] == 'a':
     # arc
     edge = { 'src': int(fields[1]),
              'dst': int(fields[2]),
-             'cap': float(fields[3]),
-             'cost': float(fields[4]) }
+             'cap_lb': int(fields[3]),
+             'cap_ub': int(fields[4]),
+             'cost': int(fields[5]) }
     edges.append(edge)
 
 dot_out(nodes, edges)
