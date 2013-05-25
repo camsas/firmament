26d20ace677f00c9b8e90465a92ba69eea6aca71
ms705 <ms705-git@srcf.ucam.org>
2013-05-25 18:07:52 +0100

    [scripts] Standardize the operation of flow scheduler plotting driver script.

--- scripts/plot_flow_graph.sh
+++ scripts/plot_flow_graph.sh
@@ -8,6 +8,6 @@ for f in `ls ${OUTDIR}/debug_*.dm`; do
   #OUTPUT="${f}_${TIMESTAMP}.png"
   DOT_FILE="${f}.gv"
   OUTPUT="${f}.png"
-  python dimacs_to_dot.py ${f} `echo ${f} | sed -e s/debug_/debug-flow_/` > ${DOT_FILE}
+  python scripts/dimacs_to_dot.py ${f} `echo ${f} | sed -e s/debug_/debug-flow_/` > ${DOT_FILE}
   dot -Tpng ${DOT_FILE} > ${OUTPUT}
 done
