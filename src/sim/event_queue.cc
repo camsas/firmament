73b85fd4be510907cc736d2ffceb5795d499d361
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2014-02-13 18:25:31 +0000

    [sim] Clean out old simulator code and add skeleton infrastructure for new simulation mode.

--- src/sim/event_queue.cc
+++ src/sim/event_queue.cc
@@ -4,35 +4,23 @@
 
 namespace firmament {
 
-void EventQueue::AddJobEvent(double time, SimEvent::SimEventType type,
-                             JobSim *job, EnsembleSim* ensemble) {
-  event_queue_.push(new SimJobEvent(time, type, ensemble, job));
-  VLOG(2) << "Added new job event of type " << type << " for job "
-          << job->name() << " at time " << time << " to event queue.";
-}
-
-void EventQueue::AddTaskEvent(double time, SimEvent::SimEventType type,
-                              JobSim *job, TaskSim *task,
-                              EnsembleSim *ensemble) {
-  event_queue_.push(new SimTaskEvent(time, type, ensemble, job, task));
-  VLOG(2) << "Added new task event of type " << type << " for task "
-          << task->name() << " in job " << job->name() << ", to be processed "
-          << "at time " << time;
-}
-
-SimEvent* EventQueue::GetNextEvent() {
+SimulationEvent* EventQueue::GetNextEvent() {
   VLOG(2) << "GetNextEvent called, queue size is " << event_queue_.size()
           << ", first event time is " << event_queue_.top()->time();
   if (event_queue_.size() > 0) {
-    SimEvent *tmp = event_queue_.top();
+    SimulationEvent* tmp = event_queue_.top();
     return tmp;
-  } else
+  } else {
     return NULL;
+  }
 }
 
 void EventQueue::PopEvent() {
-  if (event_queue_.size() > 0)
+  if (event_queue_.size() > 0) {
+    SimulationEvent* tmp = event_queue_.top();
     event_queue_.pop();
+    delete tmp;
+  }
 }
 
 }  // namespace firmament
