087d01b7984fd5bba7f3aa60ab9763fd43b942c6
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2014-11-10 18:16:12 +0100

    [sim] Update Google trace extraction tool with support for jobs.

--- src/sim/trace-extract/google_trace_extractor.h
+++ src/sim/trace-extract/google_trace_extractor.h
@@ -1,4 +1,7 @@
-// TODO
+// The Firmament project
+// Copyright (c) 2014 Malte Schwarzkopf <malte.schwarzkopf@cl.cam.ac.uk>
+//
+// Google cluster trace extractor tool.
 
 #ifndef FIRMAMENT_SIM_TRACE_EXTRACT_GOOGLE_TRACE_EXTRACTOR_H
 #define FIRMAMENT_SIM_TRACE_EXTRACT_GOOGLE_TRACE_EXTRACTOR_H
@@ -6,6 +9,7 @@
 #include "base/common.h"
 #include "base/resource_topology_node_desc.pb.h"
 #include "misc/utils.h"
+#include "misc/map-util.h"
 
 namespace firmament {
 namespace sim {
@@ -15,10 +19,21 @@ class GoogleTraceExtractor {
   explicit GoogleTraceExtractor(string& trace_path);
   void Run();
  private:
-  ResourceTopologyNodeDescriptor& LoadInitialMachines(int32_t max_num);
-  void LoadInitialJobs();
+  void AddMachineToTopology(const ResourceTopologyNodeDescriptor& machine_tmpl,
+                            ResourceTopologyNodeDescriptor* rtn_root);
+
+  ResourceTopologyNodeDescriptor& LoadInitialMachines(int64_t max_num);
+  vector<JobDescriptor*>& LoadInitialJobs(int64_t max_jobs);
   void LoadInitalTasks();
 
+  void PopulateJob(JobDescriptor* jd, uint64_t job_id);
+
+  uint64_t ReadJobsFile(vector<uint64_t>* jobs, int64_t num_jobs);
+  uint64_t ReadMachinesFile(vector<uint64_t>* machines, int64_t num_jobs);
+
+  void reset_uuid(ResourceTopologyNodeDescriptor* rtnd);
+
+  map<string, string> uuid_conversion_map_;
   string trace_path_;
 };
 
