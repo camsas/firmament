camsas/firmament

commit 94c0f56ee42e2ee49475b2c603ed4a9727b59752

[engine misc scheduling] Remove deprecated GenerateUUID() invocation from code base, and base resource ID RNG seed on listen URI, not hostname. This avoids the coordinator getting confused if parent and child coordinators are running on the same machine (e.g. for debugging).
Change-Id: Idd481c0679c18a5c67e194e5956f25761ef40cbe

src/engine/coordinator.cc
@@ -57,7 +57,7 @@ DEFINE_bool(populate_knowledge_base_from_file, false,
 namespace firmament {
 
 Coordinator::Coordinator(PlatformID platform_id)
-  : Node(platform_id, GenerateUUID()),
+  : Node(platform_id, GenerateResourceID(FLAGS_listen_uri)),
     associated_resources_(new ResourceMap_t),
     local_resource_topology_(new ResourceTopologyNodeDescriptor),
     job_table_(new JobMap_t),

src/engine/worker.cc
@@ -14,11 +14,13 @@
 #include "base/common.h"
 #include "messages/heartbeat_message.pb.h"
 #include "messages/registration_message.pb.h"
+#include "misc/utils.h"
 #include "platforms/common.pb.h"
 #include "platforms/unix/stream_sockets_adapter.h"
 
 // N.B.: We will be inheriting a bunch of standard flags from Node here (in
 // addition to those specified below).
+DECLARE_string(listen_uri);
 DEFINE_string(coordinator_uri, "", "The URI to contact the coordinator at.");
 DEFINE_string(name, "", "A friendly name for this worker.");
 
@@ -31,7 +33,7 @@ using boost::posix_time::seconds;
 #endif
 
 Worker::Worker(PlatformID platform_id)
-  : Node(platform_id, GenerateUUID()),
+  : Node(platform_id, GenerateResourceID(FLAGS_listen_uri)),
     chan_(new StreamSocketsChannel<BaseMessage>(
           StreamSocketsChannel<BaseMessage>::SS_TCP)),
     coordinator_uri_(FLAGS_coordinator_uri) {
@@ -107,11 +109,6 @@ bool Worker::ConnectToCoordinator(const string& coordinator_uri) {
     return false;
   // Send registration message
   return RegisterWithCoordinator();
-}
-
-ResourceID_t Worker::GenerateUUID() {
-  boost::uuids::random_generator gen;
-  return gen();
 }
 
 void Worker::Run() {

src/engine/Makefile
@@ -168,6 +168,7 @@ $(OBJ_DIR)/coordinator_sim: $(addprefix $(OBJ_DIR)/, $(OBJS)) \
 $(OBJ_DIR)/worker: $(addprefix $(OBJ_DIR)/, $(OBJS)) \
 	$(BUILD_DIR)/platforms/unix/libfirmament_unix.a \
 	$(BUILD_DIR)/messages/libfirmament_messages.a \
+	$(BUILD_DIR)/misc/libfirmament_misc.a \
 	$(BUILD_DIR)/storage/libfirmament_storage.a \
 	$(BUILD_DIR)/base/libfirmament_base.a
 	$(call quiet-command, \
@@ -177,6 +178,7 @@ $(OBJ_DIR)/worker: $(addprefix $(OBJ_DIR)/, $(OBJS)) \
 		$(BUILD_DIR)/engine/node.o \
 		$(BUILD_DIR)/engine/worker.o \
 		$(BUILD_DIR)/messages/libfirmament_messages.a \
+		$(BUILD_DIR)/misc/libfirmament_misc.a \
 		$(BUILD_DIR)/base/libfirmament_base.a \
 		$(BUILD_DIR)/platforms/common.pb.o \
 		$(BUILD_DIR)/platforms/unix/libfirmament_unix.a \

src/engine/simple_scheduler_test.cc
@@ -31,7 +31,7 @@ class SimpleSchedulerTest : public ::testing::Test {
   SimpleSchedulerTest() :
     job_map_(new JobMap_t),
     res_map_(new ResourceMap_t),
-    obj_store_(new store::SimpleObjectStore(GenerateUUID())),
+    obj_store_(new store::SimpleObjectStore(GenerateResourceID())),
     task_map_(new TaskMap_t) {
     // You can do set-up work for each test here.
     FLAGS_v = 3;
@@ -53,7 +53,7 @@ class SimpleSchedulerTest : public ::testing::Test {
     sched_.reset(new SimpleScheduler(job_map_, res_map_, &res_topo_,
                                      obj_store_, task_map_,
                                      shared_ptr<TopologyManager>(), NULL,
-                                     GenerateUUID(), ""));
+                                     GenerateResourceID(), "test"));
   }
 
   virtual void TearDown() {

src/engine/topology_manager.cc
@@ -96,7 +96,7 @@ vector<ResourceDescriptor> TopologyManager::FlatResourceSet() {
   // It only returns the leaf nodes in the resource tree.
   vector<ResourceDescriptor> rds;
   for (uint32_t i = 0; i < NumProcessingUnits(); ++i) {
-    ResourceID_t rid = GenerateUUID();
+    ResourceID_t rid = GenerateResourceID();
     ResourceDescriptor rd;
     rd.set_uuid(to_string(rid));
     rd.set_state(ResourceDescriptor::RESOURCE_IDLE);
@@ -117,7 +117,7 @@ void TopologyManager::MakeProtobufTree(
   string obj_id;
   if (!res_id) {
     // If this object is not already known, we generate a new resource ID.
-    ResourceID_t new_rid = GenerateUUID();
+    ResourceID_t new_rid = GenerateResourceID();
     obj_id = to_string(new_rid);
     InsertIfNotPresent(&obj_to_resourceID_, node, new_rid);
     InsertIfNotPresent(&resourceID_to_obj_, new_rid, node);

src/engine/worker.h
@@ -43,7 +43,6 @@ class Worker : public Node {
   // TODO(malte): transform this into a better representation
   string coordinator_uri_;
 
-  ResourceID_t GenerateUUID();
   void HandleIncomingMessage(BaseMessage *bm,
                              const string& remote_endpoint);
   bool RegisterWithCoordinator();

src/misc/utils.cc
@@ -70,15 +70,10 @@ int ExecutableDirectory(char *pBuf, ssize_t len) {
   return bytes;
 }
 
-// DEPRECATED wrapper for backwards compatibility
-ResourceID_t GenerateUUID() {
-  return GenerateResourceID();
-}
-
 ResourceID_t GenerateResourceID() {
   if (!resource_id_rg_init_) {
-    // TODO(malte): This crude method captures the first 100 chars of the
-    // hostname (not the FQDN). It remains to be seen if it is sufficient.
+    // In the absence of a seed, we use a crude method that captures the first
+    // 100 chars of the hostname (not the FQDN).
     SetupResourceID(&resource_id_rg_, NULL);
     resource_id_rg_init_ = true;
   }
@@ -86,18 +81,28 @@ ResourceID_t GenerateResourceID() {
   return gen();
 }
 
-void SetupResourceID(boost::mt19937 *resource_id, const char *hostname) {
+ResourceID_t GenerateResourceID(const string& seed) {
+  if (!resource_id_rg_init_) {
+    // Use the seed string to create a hash with which we seed the RNG.
+    SetupResourceID(&resource_id_rg_, seed.c_str());
+    resource_id_rg_init_ = true;
+  }
+  boost::uuids::basic_random_generator<boost::mt19937> gen(&resource_id_rg_);
+  return gen();
+}
+
+void SetupResourceID(boost::mt19937 *resource_id, const char *seed) {
   size_t hash = 42;
   char hn[100];
   bzero(&hn, 100);
-  if (hostname == NULL) {
+  if (seed == NULL) {
     gethostname(hn, 100);
   } else {
-    snprintf(hn, sizeof(hn), "%s", hn);
+    snprintf(hn, sizeof(hn), "%s", seed);
   }
   // Hash the hostname (truncated to 100 characters)
   boost::hash_combine(hash, hn);
-  VLOG(2) << "Seeing resource ID RNG with " << hash << " from hostname "
+  VLOG(2) << "Seeding resource ID RNG with " << hash << " from seed "
           << hn;
   resource_id->seed(hash);
 }

src/misc/utils.h
@@ -43,8 +43,8 @@ uint64_t GetCurrentTimestamp();
 uint64_t MakeEnsembleUID(Ensemble *ens);*/
 
 int ExecutableDirectory(char *pBuf, ssize_t len);
-ResourceID_t GenerateUUID();  // DEPRECATED, exists only for backwards compat
 ResourceID_t GenerateResourceID();
+ResourceID_t GenerateResourceID(const string& seed);
 ResourceID_t GenerateRootResourceID(const string& hostname);
 JobID_t GenerateJobID();
 JobID_t GenerateJobID(uint64_t job_id);

commit f66d7437c3ddc7433d5d0c076e09a903226ac009

Fix regression in resource ID generation.
New listen URI-based resource ID generator does not work well if listen_uri is
set to the default: all machines' resource topologies have the same IDs!

This fixes a regression introduced in
94c0f56ee42e2ee49475b2c603ed4a9727b59752.

The commit also fixes leftover uses of the deprecated GenerateUUID() call in
the flow scheduler unit tests.

Affected modules: engine scheduling/flow

Change-Id: I654d6e53df48567755b6e9e00c7547ff7cda15aa

src/engine/coordinator.cc
@@ -57,7 +57,9 @@ DEFINE_bool(populate_knowledge_base_from_file, false,
 namespace firmament {
 
 Coordinator::Coordinator(PlatformID platform_id)
-  : Node(platform_id, GenerateResourceID(FLAGS_listen_uri)),
+  : Node(platform_id,
+      GenerateResourceID(
+        boost::asio::ip::host_name() + "/" + FLAGS_listen_uri)),
     associated_resources_(new ResourceMap_t),
     local_resource_topology_(new ResourceTopologyNodeDescriptor),
     job_table_(new JobMap_t),

src/engine/worker.cc
@@ -33,7 +33,9 @@ using boost::posix_time::seconds;
 #endif
 
 Worker::Worker(PlatformID platform_id)
-  : Node(platform_id, GenerateResourceID(FLAGS_listen_uri)),
+  : Node(platform_id,
+      GenerateResourceID(
+        boost::asio::ip::host_name() + "/" + FLAGS_listen_uri)),
     chan_(new StreamSocketsChannel<BaseMessage>(
           StreamSocketsChannel<BaseMessage>::SS_TCP)),
     coordinator_uri_(FLAGS_coordinator_uri) {

src/scheduling/flow/flow_graph_test.cc
@@ -51,15 +51,15 @@ class FlowGraphTest : public ::testing::Test {
 
   // Objects declared here can be used by all tests.
   void CreateSimpleResourceTopo(ResourceTopologyNodeDescriptor *rtn_root) {
-    string root_id = to_string(GenerateUUID());
+    string root_id = to_string(GenerateResourceID());
     rtn_root->mutable_resource_desc()->set_uuid(root_id);
     ResourceTopologyNodeDescriptor* rtn_c1 = rtn_root->add_children();
-    string c1_uid = to_string(GenerateUUID());
+    string c1_uid = to_string(GenerateResourceID());
     rtn_c1->mutable_resource_desc()->set_uuid(c1_uid);
     rtn_c1->mutable_resource_desc()->set_type(ResourceDescriptor::RESOURCE_PU);
     rtn_c1->set_parent_id(root_id);
     ResourceTopologyNodeDescriptor* rtn_c2 = rtn_root->add_children();
-    string c2_uid = to_string(GenerateUUID());
+    string c2_uid = to_string(GenerateResourceID());
     rtn_c2->mutable_resource_desc()->set_uuid(c2_uid);
     rtn_c2->mutable_resource_desc()->set_type(ResourceDescriptor::RESOURCE_PU);
     rtn_c2->set_parent_id(root_id);
@@ -139,7 +139,7 @@ TEST_F(FlowGraphTest, AddResourceNode) {
   uint32_t num_changes = g.graph_changes_.size();
   // Add a new core node.
   ResourceTopologyNodeDescriptor* core_node = rtn_root.add_children();
-  string core_uid = to_string(GenerateUUID());
+  string core_uid = to_string(GenerateResourceID());
   core_node->mutable_resource_desc()->set_uuid(core_uid);
   core_node->mutable_resource_desc()->set_type(ResourceDescriptor::RESOURCE_PU);
   core_node->set_parent_id(root_id);
@@ -165,16 +165,16 @@ TEST_F(FlowGraphTest, AddResourceNode) {
 
   // Add two new PUs.
   ResourceTopologyNodeDescriptor* memory_node = rtn_root.add_children();
-  string memory_uid = to_string(GenerateUUID());
+  string memory_uid = to_string(GenerateResourceID());
   memory_node->mutable_resource_desc()->set_uuid(memory_uid);
   memory_node->set_parent_id(root_id);
   ResourceTopologyNodeDescriptor* pu1_node = memory_node->add_children();
-  string pu1_uid = to_string(GenerateUUID());
+  string pu1_uid = to_string(GenerateResourceID());
   pu1_node->mutable_resource_desc()->set_uuid(pu1_uid);
   pu1_node->mutable_resource_desc()->set_type(ResourceDescriptor::RESOURCE_PU);
   pu1_node->set_parent_id(memory_uid);
   ResourceTopologyNodeDescriptor* pu2_node = memory_node->add_children();
-  string pu2_uid = to_string(GenerateUUID());
+  string pu2_uid = to_string(GenerateResourceID());
   pu2_node->mutable_resource_desc()->set_uuid(pu2_uid);
   pu2_node->mutable_resource_desc()->set_type(ResourceDescriptor::RESOURCE_PU);
   pu2_node->set_parent_id(memory_uid);
@@ -239,7 +239,7 @@ TEST_F(FlowGraphTest, DeleteResourceNode) {
   string root_id = rtn_root.mutable_resource_desc()->uuid();
   // Add a new core node.
   ResourceTopologyNodeDescriptor* core_node = rtn_root.add_children();
-  string core_uid = to_string(GenerateUUID());
+  string core_uid = to_string(GenerateResourceID());
   core_node->mutable_resource_desc()->set_uuid(core_uid);
   core_node->mutable_resource_desc()->set_type(ResourceDescriptor::RESOURCE_PU);
   core_node->set_parent_id(root_id);