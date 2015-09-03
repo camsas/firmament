b4f5207567f3b969c3b9ae14be40cbc99437a5ec
Ionel Gog <gogionel@gmail.com>
2015-09-03 09:12:46 +0100

    Updated SimulatedMessagingAdapter to respect Google style guide.
    This change moves the code to the header file.
    
    Affected modules: platforms/sim
    
    Change-Id: Iafb22f8e0b2e100fc7c82a9fb74bc7bb376ca763

--- src/platforms/sim/simulated_messaging_adapter.h
+++ src/platforms/sim/simulated_messaging_adapter.h
@@ -6,8 +6,12 @@
 
 #include "misc/messaging_interface.h"
 
+#include <string>
+
+#include "messages/base_message.pb.h"
+
 namespace firmament {
-namespace platforms {
+namespace platform {
 namespace sim {
 
 template <class T>
@@ -15,23 +19,58 @@ class SimulatedMessagingAdapter : public MessagingAdapterInterface<T>,
   public boost::enable_shared_from_this<SimulatedMessagingAdapter<T> >,
   private boost::noncopyable {
  public:
-  void AwaitNextMessage();
-  void CloseChannel(MessagingChannelInterface<T>* channel);
+  void AwaitNextMessage() {
+    // No-op.
+  }
+
+  void CloseChannel(MessagingChannelInterface<T>* channel) {
+    // No-op.
+    // We don't have to close the channel because we haven't established it.
+  }
+
   bool EstablishChannel(const string& endpoint_uri,
-                        MessagingChannelInterface<T>* chan);
-  MessagingChannelInterface<T>* GetChannelForEndpoint(const string& endpoint);
-  void ListenURI(const string& endpoint_uri);
-  bool ListenReady();
+                        MessagingChannelInterface<T>* chan) {
+    return true;
+  }
+
+  MessagingChannelInterface<T>* GetChannelForEndpoint(const string& endpoint) {
+    // No-op.
+    return NULL;
+  }
+
+  void ListenURI(const string& endpoint_uri) {
+    // No-op.
+  }
+
+  bool ListenReady() {
+    return true;
+  }
+
   void RegisterAsyncMessageReceiptCallback(
-      typename AsyncMessageRecvHandler<T>::type callback);
+      typename AsyncMessageRecvHandler<T>::type callback) {
+    // No-op.
+    // Callback not required.
+  }
+
   void RegisterAsyncErrorPathCallback(
-      typename AsyncErrorPathHandler<T>::type callback);
-  bool SendMessageToEndpoint(const string& endpoint_uri, T& message);  // NOLINT
-  ostream& ToString(ostream* stream) const;
+      typename AsyncErrorPathHandler<T>::type callback) {
+    // No-op.
+    // We don't simulate any errors.
+  }
+
+  bool SendMessageToEndpoint(const string& endpoint_uri, T& message) { // NOLINT
+    // No-op.
+    return true;
+  }
+
+  ostream& ToString(ostream* stream) const {
+    // No-op.
+    return *stream << "(MessagingAdapter,type=Simulated,at=" << this << ")";
+  }
 };
 
 }  // namespace sim
-}  // namespace platforms
+}  // namespace platform
 }  // namespace firmament
 
 #endif  // FIRMAMENT_PLATFORMS_SIM_SIMULATED_MESSAGING_ADAPTER_H
