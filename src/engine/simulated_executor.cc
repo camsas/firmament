6c26b9f1da40bc8eebf1f25da7ef0752b3110582
Ionel Gog <gogionel@gmail.com>
2015-09-03 08:45:12 +0100

    Populate the task report in the simulator executor.
    This change adds task runtime related information to the task report.
    
    Affected modules: engine
    
    Change-Id: I9dcdeb8b8a8d4cb60f568cf4bdbf997f3f67e006

--- src/engine/simulated_executor.cc
+++ src/engine/simulated_executor.cc
@@ -21,23 +21,28 @@ bool SimulatedExecutor::CheckRunningTasksHealth(
   return true;
 }
 
-void SimulatedExecutor::HandleTaskCompletion(TaskDescriptor* td,
-                                             TaskFinalReport* report) {
+void SimulatedExecutor::HandleTaskCompletion(TaskDescriptor* td_ptr,
+                                             TaskFinalReport* task_report) {
+  // NOTE: We do not have information to set instructions, cycles, llc_refs
+  // and llcs_misses.
+  task_report->set_task_id(td_ptr->uid());
+  task_report->set_start_time(td_ptr->start_time());
+  task_report->set_finish_time(td_ptr->finish_time());
+  task_report->set_runtime(td_ptr->finish_time() -
+                           td_ptr->start_time());
+}
+
+void SimulatedExecutor::HandleTaskEviction(TaskDescriptor* td_ptr) {
   // No-op.
   // The simulator is responsible for updating the task's state.
 }
 
-void SimulatedExecutor::HandleTaskEviction(TaskDescriptor* td) {
+void SimulatedExecutor::HandleTaskFailure(TaskDescriptor* td_ptr) {
   // No-op.
   // The simulator is responsible for updating the task's state.
 }
 
-void SimulatedExecutor::HandleTaskFailure(TaskDescriptor* td) {
-  // No-op.
-  // The simulator is responsible for updating the task's state.
-}
-
-void SimulatedExecutor::RunTask(TaskDescriptor* td,
+void SimulatedExecutor::RunTask(TaskDescriptor* td_ptr,
                                 bool firmament_binary) {
   // No-op.
   // The simulator is responsible for setting task start time.
