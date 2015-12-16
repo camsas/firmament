2e4cd9df99e2ad04073b5063b50e215b46dc53a5
Ionel Gog <gogionel@gmail.com>
2015-12-16 12:34:00 +0000

    Updated DIMACS node types to match the types expected by the solver.
    This change uniformizes the types of nodes exported in the extended
    DIMACS format and the node types expected by the solver.
    
    Affected modules: scheduling/flow
    
    Change-Id: I7edbed0fdc72619a0b4f3c2f12844b797bf7ede7

--- src/scheduling/flow/dimacs_add_node.cc
+++ src/scheduling/flow/dimacs_add_node.cc
@@ -8,6 +8,17 @@
 
 namespace firmament {
 
+// Node type is used to construct the mapping of tasks to PUs in the solver.
+// NOTE: Do not reorder types because it will affect the communication with
+// the solver.
+
+enum NodeType {
+  DIMACS_NODE_OTHER = 0,
+  DIMACS_NODE_TASK = 1,
+  DIMACS_NODE_PU = 2,
+  DIMACS_NODE_SINK = 3
+};
+
 DIMACSAddNode::DIMACSAddNode(const FlowGraphNode& node,
                              const vector<FlowGraphArc*>& arcs) :
      DIMACSChange(), id_(node.id_), excess_(node.excess_), type_(node.type_) {
@@ -30,15 +41,15 @@ const string DIMACSAddNode::GenerateChange() const {
 
 uint32_t DIMACSAddNode::GetNodeType() const {
   if (type_ == FlowNodeType::PU) {
-    return 2;
+    return DIMACS_NODE_PU;
   } else if (type_ == FlowNodeType::SINK) {
-    return 3;
+    return DIMACS_NODE_SINK;
   } else if (type_ == FlowNodeType::UNSCHEDULED_TASK ||
              type_ == FlowNodeType::SCHEDULED_TASK ||
              type_ == FlowNodeType::ROOT_TASK) {
-    return 1;
+    return DIMACS_NODE_TASK;
   } else {
-    return 0;
+    return DIMACS_NODE_OTHER;
   }
 }
 
