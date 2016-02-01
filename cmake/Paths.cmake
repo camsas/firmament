e6bdbe685bc7f60f41b2fe4a70b8ea597d7cc6c3
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2016-02-01 18:01:50 +0100

    Introduce CMake-based build system.
    This change starts the migration of our build system to CMake.
    Currently, the CMake setup can build and link the coordinator and all of
    its dependencies. Other targets (and automatic installation of external
    dependencies) are still missing.
    
    Change-Id: I8978444bcb40d67927f991bc9f5eb575319e7571

---
+++ cmake/Paths.cmake
@@ -0,0 +1,12 @@
+# The initial SOURCE_DIR is the root of the source tree, where the main
+# CMakeLists.txt file lives
+set(Firmament_ROOT_DIR ${Firmament_SOURCE_DIR})
+
+# CMake modules
+set(CMAKE_MODULE_PATH ${Firmament_ROOT_DIR}/cmake)
+
+# Reset the source root
+set(Firmament_SOURCE_DIR ${Firmament_ROOT_DIR}/src)
+
+# Convenience variable for the build root
+set(Firmament_BUILD_DIR ${Firmament_BINARY_DIR})
