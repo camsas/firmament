68bd8974bab4b9ef5ea4bf3af685c1f91577dbb0
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2016-03-03 10:26:45 -0500

    Fix Docker image build for new build system.
    This change updates the Dockerfile and the image building scripts
    to work with the new CMake-based build system.
    
    Affected modules: contrib
    
    Change-Id: I03e180c60657df40921c40dfd2827de1bb877dfa

--- contrib/docker-start.sh
+++ contrib/docker-start.sh
@@ -2,4 +2,4 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # Make the docker image and tag it as "firmament"
-docker run --expose=8080 --workdir=/firmament firmament build/engine/coordinator --flagfile=/firmament/default.conf
+docker run --expose=8080 --workdir=/firmament firmament src/coordinator --flagfile=/firmament/default.conf
