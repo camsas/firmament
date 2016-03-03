camsas/firmament

commit f0f38b7ccce68cc2e8ec25559882e4e3ace23407

Fixed compiler warnings.
We now have no compiler warnings which will make it easier to spot any
compilation issues. This change fixes almost all unused-result and
unused-function in the Firmament code base. However, I still had to
disable the warnings because pb2json has several methods that trigger
these warnings.

Affected modules: engine platforms/unix scheduling/flow sim

Change-Id: Ibca1fc88cbe3d1e213727037058e05bedbaf7202

src/platforms/unix/procfs_machine.cc
@@ -247,8 +247,8 @@ MemoryStatistics_t ProcFSMachine::GetMemoryStats() {
   while (!feof(mem_stat_fd)) {
     char label[100];
     uint64_t val = 0;
-    fscanf(mem_stat_fd, "%s", label);
-    fscanf(mem_stat_fd, "%ju", &val);
+    CHECK_EQ(fscanf(mem_stat_fd, "%s", label), 1);
+    CHECK_EQ(fscanf(mem_stat_fd, "%ju", &val), 1);
     if (strncmp(label, "MemTotal:", 100) == 0) {
       mem_stats.mem_total = val * 1024;
     } else if (strncmp(label, "MemFree:", 100) == 0) {

cmake/Config.cmake
@@ -12,5 +12,3 @@ if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
 else()
   # other compilers, usually g++
 endif()
-
-

src/engine/executors/local_executor.cc
@@ -302,7 +302,6 @@ int32_t LocalExecutor::RunProcessSync(TaskID_t task_id,
                                       bool debug,
                                       bool default_args,
                                       const string& tasklog) {
-  char* perf_prefix;
   pid_t pid;
   /*int pipe_to[2];    // pipe to feed input data to task
   int pipe_from[3];  // pipe to receive output data from task
@@ -323,12 +322,12 @@ int32_t LocalExecutor::RunProcessSync(TaskID_t task_id,
     // task debugging is active, so reserve extra space for the
     // gdb invocation prefix.
     argv.reserve(args.size() + (default_args ? 4 : 3));
-    perf_prefix = AddDebuggingToCommandLine(&argv);
+    AddDebuggingToCommandLine(&argv);
   } else if (perf_monitoring) {
     // performance monitoring is active, so reserve extra space for the
     // "perf" invocation prefix.
     argv.reserve(args.size() + (default_args ? 11 : 10));
-    perf_prefix = AddPerfMonitoringToCommandLine(env, &argv);
+    AddPerfMonitoringToCommandLine(env, &argv);
   } else {
     // no performance monitoring, so we only need to reserve space for the
     // default and NULL args
@@ -393,7 +392,7 @@ int32_t LocalExecutor::RunProcessSync(TaskID_t task_id,
         PLOG(FATAL) << "Failed to close stderr FD in child";
 
       // Change to task's working directory
-      chdir(env["FLAGS_task_data_dir"].c_str());
+      CHECK_EQ(chdir(env["FLAGS_task_data_dir"].c_str()), 0);
 
       // Close the open FDs in the child before exec-ing, so that the task does
       // not inherit all of the coordinator's sockets and FDs.

src/platforms/unix/procfs_machine.h
@@ -61,7 +61,7 @@ class ProcFSMachine {
   NetworkStatistics_t GetNetworkStats();
 
   inline void readunsigned(FILE* input, uint64_t *x) {
-    fscanf(input, "%ju ", x);
+    CHECK_EQ(fscanf(input, "%ju ", x), 1);
   }
 
   vector<CPUStatistics_t> cpu_stats_;

src/platforms/unix/procfs_monitor.h
@@ -108,12 +108,18 @@ class ProcFSMonitor {
   // Find a line matching the regular expression provided
   vector<string>* FindMatchingLine(const string& regexp, const string& data);
   void GetStatsForPID(pid_t pid, ProcessStatistics_t* stats);
-  inline void readone(FILE* input, uint64_t *x) { fscanf(input, "%ju ", x); }
-  inline void readunsigned(FILE* input, uint64_t *x) {
-    fscanf(input, "%ju ", x);
+  inline void readone(FILE* input, uint64_t *x) {
+    CHECK_EQ(fscanf(input, "%ju ", x), 1);
   }
-  inline void readstr(FILE* input, char *x) {  fscanf(input, "%s ", x); }
-  inline void readchar(FILE* input, char *x) {  fscanf(input, "%c ", x); }
+  inline void readunsigned(FILE* input, uint64_t *x) {
+    CHECK_EQ(fscanf(input, "%ju ", x), 1);
+  }
+  inline void readstr(FILE* input, char *x) {
+    CHECK_EQ(fscanf(input, "%s ", x), 1);
+  }
+  inline void readchar(FILE* input, char *x) {
+    CHECK_EQ(fscanf(input, "%c ", x), 1);
+  }
   inline void skip(FILE* input, int64_t num) {  fseek(input, num, SEEK_CUR); }
 };
 

src/platforms/unix/procfs_monitor_test.cc
@@ -57,7 +57,7 @@ TEST_F(ProcFSMonitorTest, SimpleProcessStatsTest) {
   const ProcFSMonitor::ProcessStatistics_t* stats =
       pfsm_.ProcessInformation(pid, NULL);
   CHECK_EQ(stats->pid, pid);
-  malloc(getpagesize());
+  CHECK_NOTNULL(malloc(getpagesize()));
   // Should have more pages allocated now...
   CHECK_LT(stats->rss, pfsm_.ProcessInformation(pid, NULL)->rss);
 }

src/platforms/unix/stream_sockets_adapter.h
@@ -59,7 +59,6 @@ class StreamSocketsAdapter : public firmament::MessagingAdapterInterface<T>,
       return;
     // Otherwise, let's make sure we have an outstanding async receive request
     // for each fo them.
-    bool any_outstanding = false;
     {
       boost::lock_guard<boost::mutex> envel_lock(channel_recv_envelopes_mutex_);
       for (__typeof__(endpoint_channel_map_.begin()) chan_iter =
@@ -79,7 +78,6 @@ class StreamSocketsAdapter : public firmament::MessagingAdapterInterface<T>,
                                   boost::asio::placeholders::error,
                                   boost::asio::placeholders::bytes_transferred,
                                   chan));
-          any_outstanding = true;
         }
       }
     }

src/scheduling/flow/solver_dispatcher.cc
@@ -199,7 +199,7 @@ multimap<uint64_t, uint64_t>* SolverDispatcher::Run(
 
   // Now run the solver
   vector<string> args;
-  pid_t solver_pid;
+  pid_t solver_pid = 0;
   pthread_t logger_thread = -1;
   // If the solver hasn't executed or if we're not running in incremental mode.
   if (!solver_ran_once_ || !FLAGS_incremental_flow) {

commit 66883b74276a45c496438fdf4837f76f8e1e42b7

Fix error in parsing /proc/meminfo.
This fixed a bug introduced by f0f38b7ccce68cc2e8ec25559882e4e3ace23407
caused by the fact that not all lines in /proc/meminfo match our
express (%s %j). Previously, such lines would have been silently
ignored, but the CHECK caused things to fail if a line did not match.

Affected modules: platforms/unix

Change-Id: I64a3a03fd8ba57653db0c9970b754653a9f2c21f

src/platforms/unix/procfs_machine.cc
@@ -247,8 +247,8 @@ MemoryStatistics_t ProcFSMachine::GetMemoryStats() {
   while (!feof(mem_stat_fd)) {
     char label[100];
     uint64_t val = 0;
-    CHECK_EQ(fscanf(mem_stat_fd, "%s", label), 1);
-    CHECK_EQ(fscanf(mem_stat_fd, "%ju", &val), 1);
+    CHECK_GE(fscanf(mem_stat_fd, "%s", label), 0);
+    CHECK_GE(fscanf(mem_stat_fd, "%ju", &val), 0);
     if (strncmp(label, "MemTotal:", 100) == 0) {
       mem_stats.mem_total = val * 1024;
     } else if (strncmp(label, "MemFree:", 100) == 0) {