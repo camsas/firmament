68fbeca7be8a7d952fb2635de0efdcc732d8eaac
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2016-03-09 21:44:13 -0500

    Correct CMake compiler flags for debug/release.
    This change fixes the use of "-g -O0" for builds with DEBUG=1 set.
    Previously, we would always have "-03" set, which made debugging more
    difficult.
    
    Change-Id: I28c0e006f991db0403c161fa68e40017b33ee8e1

--- cmake/Config.cmake
+++ cmake/Config.cmake
@@ -1,8 +1,16 @@
 include_directories(${Firmament_SOURCE_DIR})
 include_directories(${Firmament_BUILD_DIR}/src)
 
+# debug/release flags
+if(DEBUG)
+  set(CMAKE_CXX_FLAGS "-g -O0 -fsanitize=address")
+else()
+  set(CMAKE_CXX_FLAGS "-O3")
+endif()
+
 # Shared compiler flags used by all builds
-set(CMAKE_CXX_FLAGS "-O3 -std=c++11")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wno-long-long -Wno-variadic-macros -Wno-deprecated -Wno-vla -Wno-unused-parameter -Wno-error=unused-parameter -Wno-error=unused-function")
 
 # Compiler-specific flags
