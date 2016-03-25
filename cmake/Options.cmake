1e24117849f70d8eac975bb3a507111e36363c1f
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2016-03-25 22:23:07 +0000

    Add CMake flag to toggle building unit tests.
    This change adds a flag to the build system that allows to control
    whether the unit test binaries are built (on by default). This allows
    for quicker builds if no unit tests are desired.
    
    Affected modules: base engine misc platforms scheduling sim storage
    
    Change-Id: I864edd17a0accebcab5372c4f55ebe021b709e78

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -5,8 +5,9 @@ option(VERBOSE "Enable verbose compilation output" OFF)
 # Modular components
 option(BUILD_SIMULATOR "Builds the trace-driven simulation front-end for Firmament." ON)
 option(BUILD_EXAMPLES "Builds the example binaries linked with TaskLib." ON)
-option(BUILD_SHUTTLE "Builds the Shuttle cluster manager front-end." ON)
+option(BUILD_COORDINATOR "Builds the Shuttle cluster manager front-end." ON)
 option(BUILD_TASKLIB "Builds the TaskLib shared library for use with Shuttle." ON)
+option(BUILD_TESTS "Builds the unit tests." ON)
 
 # Solver options
 option(ENABLE_CS2 "Downloads, compiles and enables the cs2 solver (academic license only)." ON)
