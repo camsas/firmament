8229326466b6bac59c6dc22c949f50405fc41b62
Ionel Gog <gogionel@gmail.com>
2016-03-28 20:35:06 -0400

    Added simulated bounded DFS.
    This change adds a simulated DFS in which a job's blocks are
    only placed on #tasks_job_has machines.
    
    Moreover, the change introduces a SimulatedDFSInterface which
    every simulated DFS has to implement.
    
    Affected modules: sim sim/dfs
    
    Change-Id: I05732be672c1aa0b31f1445063c3ab0b4bde8cd0

---
+++ src/sim/dfs/simulated_bounded_dfs.h
@@ -0,0 +1,29 @@
+// The Firmament project
+// Copyright (c) 2016 Ionel Gog <ionel.gog@cl.cam.ac.uk>
+
+#ifndef FIRMAMENT_SIM_DFS_SIMULATED_BOUNDED_DFS_H
+#define FIRMAMENT_SIM_DFS_SIMULATED_BOUNDED_DFS_H
+
+#include "sim/dfs/simulated_uniform_dfs.h"
+
+namespace firmament {
+namespace sim {
+
+class SimulatedBoundedDFS : public SimulatedUniformDFS {
+ public:
+  SimulatedBoundedDFS(TraceGenerator* trace_generator);
+  ~SimulatedBoundedDFS();
+
+  void AddBlocksForTask(const TaskDescriptor& td, uint64_t num_blocks,
+                        uint64_t max_machine_spread);
+ private:
+  ResourceID_t PlaceBlockOnMachinesPool(TaskID_t task_id, uint64_t block_id,
+                                        vector<ResourceID_t>* machines);
+  void GetJobMachinePool(const string& job_id, uint64_t num_tasks,
+                         vector<ResourceID_t>* machines);
+};
+
+} // namespace sim
+} // namespace firmament
+
+#endif // FIRMAMENT_SIM_DFS_SIMULATED_BOUNDED_DFS_H
