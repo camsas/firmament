camsas/firmament

commit 617811539445576ddd0a8b53bf8865f47ff938fc

Task duration oracle support in GoogleTraceLoader.
This commit adds support for the GoogleTraceLoader to pass accurate
task runtime information into the KB. This is useful for evaluating
scheduler performance in an idealized "oracle" scenario where all
runtimes are known ahead of time. We already supported it in the
synthetic trace loader, but not in the GoogleTraceLoader.

The change necessitated a slight change to the interface for
LoadTaskRuntimeStats: it now also receives a reference to the (already
populated) task runtime map.

Affected modules: sim

Change-Id: I9c9f07d8d247a7d16261a5bf95fb96efe85174fa

src/sim/google_trace_loader.cc
@@ -56,6 +56,7 @@ DEFINE_uint64(synthetic_task_runtime, 1000000,
 
 DECLARE_uint64(runtime);
 DECLARE_double(trace_speed_up);
+DECLARE_bool(task_duration_oracle);
 
 static bool ValidateTracePath(const char* flagname, const string& trace_path) {
   if (trace_path.empty()) {
@@ -284,7 +285,8 @@ bool GoogleTraceLoader::LoadTaskEvents(
 }
 
 void GoogleTraceLoader::LoadTaskUtilizationStats(
-    unordered_map<TaskID_t, TraceTaskStats>* task_id_to_stats) {
+    unordered_map<TaskID_t, TraceTaskStats>* task_id_to_stats,
+    const unordered_map<TaskID_t, uint64_t>& task_runtimes) {
   char line[MAX_LINE_LENGTH];
   vector<string> cols;
   FILE* usage_file = NULL;
@@ -316,6 +318,7 @@ void GoogleTraceLoader::LoadTaskUtilizationStats(
         TraceTaskIdentifier ti;
         ti.job_id = lexical_cast<uint64_t>(cols[0]);
         ti.task_index = lexical_cast<uint64_t>(cols[1]);
+        TaskID_t tid = GenerateTaskIDFromTraceIdentifier(ti);
         TraceTaskStats task_stats;
         task_stats.avg_mean_cpu_usage_ = lexical_cast<double>(cols[4]);
         task_stats.avg_canonical_mem_usage_ = lexical_cast<double>(cols[8]);
@@ -327,9 +330,13 @@ void GoogleTraceLoader::LoadTaskUtilizationStats(
         task_stats.avg_cpi_ = lexical_cast<double>(cols[32]);
         task_stats.avg_mai_ = lexical_cast<double>(cols[36]);
 
-        if (!InsertIfNotPresent(task_id_to_stats,
-                                GenerateTaskIDFromTraceIdentifier(ti),
-                                task_stats) &&
+        if (FLAGS_task_duration_oracle) {
+          uint64_t runtime = 0;
+          CHECK(FindCopy(task_runtimes, tid, &runtime));
+          task_stats.total_runtime_ = runtime;
+        }
+
+        if (!InsertIfNotPresent(task_id_to_stats, tid, task_stats) &&
             VLOG_IS_ON(1)) {
           LOG(ERROR) << "LoadTaskUtilizationStats: There should not be more "
                      << "than an entry for job " << ti.job_id

src/sim/google_trace_loader.h
@@ -66,7 +66,8 @@ class GoogleTraceLoader : public TraceLoader {
                       unordered_map<uint64_t, uint64_t>* job_num_tasks);
 
   void LoadTaskUtilizationStats(
-      unordered_map<TaskID_t, TraceTaskStats>* task_id_to_stats);
+      unordered_map<TaskID_t, TraceTaskStats>* task_id_to_stats,
+      const unordered_map<TaskID_t, uint64_t>& task_runtimes);
 
   /**
    * Loads all the task runtimes and returns map task_identifier -> runtime.

src/sim/simulator_bridge.cc
@@ -364,7 +364,7 @@ void SimulatorBridge::LoadTraceData(TraceLoader* trace_loader) {
   // Load tasks' runtime.
   trace_loader->LoadTasksRunningTime(&task_runtime_);
   // Populate the knowledge base.
-  trace_loader->LoadTaskUtilizationStats(&task_id_to_stats_);
+  trace_loader->LoadTaskUtilizationStats(&task_id_to_stats_, task_runtime_);
 }
 
 void SimulatorBridge::ProcessSimulatorEvents(uint64_t events_up_to_time) {

src/sim/synthetic_trace_loader.cc
@@ -195,7 +195,8 @@ bool SyntheticTraceLoader::LoadTaskEvents(
 }
 
 void SyntheticTraceLoader::LoadTaskUtilizationStats(
-    unordered_map<TaskID_t, TraceTaskStats>* task_id_to_stats) {
+    unordered_map<TaskID_t, TraceTaskStats>* task_id_to_stats,
+    const unordered_map<TaskID_t, uint64_t>& task_runtimes) {
   uint64_t usec_between_jobs = FLAGS_synthetic_job_interarrival_time;
   TraceTaskStats task_stats;
   task_stats.avg_mean_cpu_usage_ = 0.5;
@@ -210,14 +211,14 @@ void SyntheticTraceLoader::LoadTaskUtilizationStats(
     for (uint64_t task_index = 1;
          task_index <= num_tasks_at_beginning;
          ++task_index) {
+      TaskID_t tid = GenerateTaskIDFromTraceIdentifier(task_identifier);
       task_identifier.task_index = task_index;
       if (FLAGS_task_duration_oracle) {
-        task_stats.total_runtime_ =
-          FLAGS_prepopulated_task_duration / FLAGS_trace_speed_up;
+        uint64_t runtime = 0;
+        CHECK(FindCopy(task_runtimes, tid, &runtime));
+        task_stats.total_runtime_ = runtime;
       }
-      CHECK(InsertIfNotPresent(
-          task_id_to_stats, GenerateTaskIDFromTraceIdentifier(task_identifier),
-          task_stats));
+      CHECK(InsertIfNotPresent(task_id_to_stats, tid, task_stats));
     }
   }
   uint64_t job_id = 1;
@@ -229,13 +230,13 @@ void SyntheticTraceLoader::LoadTaskUtilizationStats(
     for (uint64_t task_index = 1; task_index <= FLAGS_synthetic_tasks_per_job;
          ++task_index) {
       task_identifier.task_index = task_index;
+      TaskID_t tid = GenerateTaskIDFromTraceIdentifier(task_identifier);
       if (FLAGS_task_duration_oracle) {
-        task_stats.total_runtime_ =
-          FLAGS_synthetic_task_duration / FLAGS_trace_speed_up;
+        uint64_t runtime = 0;
+        CHECK(FindCopy(task_runtimes, tid, &runtime));
+        task_stats.total_runtime_ = runtime;
       }
-      CHECK(InsertIfNotPresent(
-          task_id_to_stats, GenerateTaskIDFromTraceIdentifier(task_identifier),
-          task_stats));
+      CHECK(InsertIfNotPresent(task_id_to_stats, tid, task_stats));
     }
   }
 }

src/sim/synthetic_trace_loader.h
@@ -37,7 +37,8 @@ class SyntheticTraceLoader : public TraceLoader {
   bool LoadTaskEvents(uint64_t events_up_to_time,
                       unordered_map<uint64_t, uint64_t>* job_num_tasks);
   void LoadTaskUtilizationStats(
-      unordered_map<TaskID_t, TraceTaskStats>* task_id_to_stats);
+      unordered_map<TaskID_t, TraceTaskStats>* task_id_to_stats,
+      const unordered_map<TaskID_t, uint64_t>& task_runtimes);
   void LoadTasksRunningTime(
       unordered_map<TaskID_t, uint64_t>* task_runtime);
  private:

src/sim/trace_loader.h
@@ -61,7 +61,8 @@ class TraceLoader {
       unordered_map<uint64_t, uint64_t>* job_num_tasks) = 0;
 
   virtual void LoadTaskUtilizationStats(
-      unordered_map<TaskID_t, TraceTaskStats>* task_id_to_stats) = 0;
+      unordered_map<TaskID_t, TraceTaskStats>* task_id_to_stats,
+      const unordered_map<TaskID_t, uint64_t>& task_runtimes) = 0;
 
   /**
    * Loads all the task runtimes and returns map task_identifier -> runtime.

commit ebba19a8f4ccba5330dc305044b146170cb744c1

Also subsample task utilization stats events.
Previously, the Google trace loader did not subsample task utilization
information extracted from the "task_usage" table, but it did subsample
other types of events. This becomes a problem when using the task
duration oracle mode introduced in
617811539445576ddd0a8b53bf8865f47ff938fc, because the code in
`LoadTaskUtilizationStats` will look for runtimes of tasks whose runtime
events were discarded earlier. The correct fix is to also subsample the
utilization events based on the task IDs that are filtered.

Affected modules: sim

Change-Id: I569ea668cf71c4975381e8c60c75690b6e04640a

src/sim/google_trace_loader.cc
@@ -320,6 +320,14 @@ void GoogleTraceLoader::LoadTaskUtilizationStats(
         ti.job_id = lexical_cast<uint64_t>(cols[0]);
         ti.task_index = lexical_cast<uint64_t>(cols[1]);
         TaskID_t tid = GenerateTaskIDFromTraceIdentifier(ti);
+
+        // Sub-sample the trace if we only retain < 100% of tasks.
+        if (SpookyHash::Hash64(&ti, sizeof(ti), kSeed) >
+            MaxEventHashToRetain()) {
+          // skip event
+          continue;
+        }
+
         TraceTaskStats task_stats;
         task_stats.avg_mean_cpu_usage_ = lexical_cast<double>(cols[4]);
         task_stats.avg_canonical_mem_usage_ = lexical_cast<double>(cols[8]);
@@ -421,15 +429,15 @@ void GoogleTraceLoader::LoadTasksRunningTime(
           // skip event
           continue;
         }
+
         // Get the total runtime of the task. This includes the time
         // of the runs that failed or were killed. In this way, we make
         // sure that the task runs for the same amount of time as when
         // it executed in real-world.
         uint64_t runtime = lexical_cast<uint64_t>(cols[4]);
         runtime /= FLAGS_trace_speed_up;
-        if (!InsertIfNotPresent(task_runtime,
-                                GenerateTaskIDFromTraceIdentifier(ti),
-                                runtime) &&
+        TaskID_t tid = GenerateTaskIDFromTraceIdentifier(ti);
+        if (!InsertIfNotPresent(task_runtime, tid, runtime) &&
             VLOG_IS_ON(1)) {
           LOG(ERROR) << "LoadTasksRunningTime: There should not be more than "
                      << "one entry for job " << ti.job_id