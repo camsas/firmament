b9289692a27aaf457f68cbff9baaf263bd9c310e
Eric Coan <ecoan@instructure.com>
2017-01-19 13:40:34 -0700

    fix networked_regex to allow hostnames
    during my testing I tried entering a "parent_uri" of a particular
    hostname. One formatted like:
    
    ```
    ${appName}-${env}.${region}.${zone}
    ```
    
    (which might look something like:
    
    ```
    firmament-test.us-west-2.zone.com
    ```
    ).
    
    this resulted in several logs saying "no hostname could be matched",
    and "no port could be matched". After far too long messing around with
    regex I was able to create some that matches both ip addresses,
    as well as hostnames (e.g. the exmaple uri above).
    
    Change-Id: Ic6160eb6731a0f570737ef57dfde0cd7848f03e2

--- src/misc/uri_tools.h
+++ src/misc/uri_tools.h
@@ -28,7 +28,7 @@
 
 namespace firmament {
 
-#define NETWORKED_REGEX "^(tcp|udp):([a-zA-Z0-9\\.]+):([0-9]+)$"
+#define NETWORKED_REGEX "^(tcp|udp):([a-zA-Z0-9\\.\\-]+):([0-9]+)$"
 
 class URITools {
  public:
