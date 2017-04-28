a013f2ec6b980f06442a73f0b766408fe24c14c8
Ionel Gog <gogionel@gmail.com>
2017-04-28 11:45:56 +0300

    Added Firmament GRPC scheduler service.
    This change exposes the Firmament service as a GRPC service.
    External cluster managers can use this service to interact
    with Firmament and get scheduling placements.
    
    Affected modules: misc scheduling
    
    Change-Id: I1dd7a5ef74294c2fd2640bcbf62b05d50a2d8a08

---
+++ src/scheduling/knowledge_base_populator.h
@@ -0,0 +1,49 @@
+/*
+ * Poseidon
+ * Copyright (c) The Poseidon Authors.
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
+ * LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR
+ * A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+ *
+ * See the Apache Version 2.0 License for specific language governing
+ * permissions and limitations under the License.
+ */
+
+#ifndef FIRMAMENT_SCHEDULING_KNOWLEDGE_BASE_POPULATOR_H
+#define FIRMAMENT_SCHEDULING_KNOWLEDGE_BASE_POPULATOR_H
+
+#include "base/machine_perf_statistics_sample.pb.h"
+#include "base/task_final_report.pb.h"
+#include "base/task_perf_statistics_sample.pb.h"
+#include "misc/wall_time.h"
+#include "scheduling/firmament_scheduler.pb.h"
+#include "scheduling/knowledge_base.h"
+
+namespace firmament {
+
+class KnowledgeBasePopulator {
+ public:
+  KnowledgeBasePopulator(boost::shared_ptr<KnowledgeBase> knowledge_base);
+  void AddMachineCpuUsage(MachinePerfStatisticsSample* machine_sample);
+  void PopulateNodeStats(const ResourceID_t& res_id,
+                         const ResourceStats* resource_stats);
+  void PopulateTaskStats(TaskID_t task_id,
+                         const TaskStats* task_stats);
+  void PopulateTaskFinalReport(const TaskDescriptor& td,
+                               TaskFinalReport* report);
+ private:
+  boost::shared_ptr<KnowledgeBase> knowledge_base_;
+  WallTime time_manager_;
+};
+
+}  // namespace firmament
+#endif  // FIRMAMENT_SCHEDULING_KNOWLEDGE_BASE_POPULATOR_H
