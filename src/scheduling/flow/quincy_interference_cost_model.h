ef5fce33123a0782113a53a563a9337a008e6fa8
michaelschaarschmidt <mi.schaarschmidt@gmail.com>
2017-06-26 08:54:45 +0100

    Implemented prototype of Quincy interference cost model.
    Costs are adjusted using a linear function of the number of running tasks
    per resource.
    
    Affected modules: scheduling
    
    Change-Id: Id5257caf56f0757cd40d5514530a3473d70b5f6c

--- src/scheduling/flow/quincy_interference_cost_model.h
+++ src/scheduling/flow/quincy_interference_cost_model.h
@@ -79,8 +79,110 @@ class QuincyInterferenceCostModel : public CostModelInterface {
   FlowGraphNode* GatherStats(FlowGraphNode* accumulator, FlowGraphNode* other);
   void PrepareStats(FlowGraphNode* accumulator);
   FlowGraphNode* UpdateStats(FlowGraphNode* accumulator, FlowGraphNode* other);
+  double InterferenceCostFactor(ResourceID_t resource_id);
 
- private:
+private:
+  uint64_t ComputeClusterDataStatistics(
+      TaskDescriptor* td_ptr,
+      unordered_map<ResourceID_t, uint64_t,
+        boost::hash<ResourceID_t>>* data_on_machines,
+      unordered_map<EquivClass_t, uint64_t>* data_on_racks);
+  /**
+   * Compute the amount of data the task has on the machine given as argument.
+   * @param td the descriptor of the task for which to compute statistics
+   * @param machine_res_id resource id of the machine we're computing stats for
+   * @param data_on_rack the amount of unique data the task has on the
+   * machine's rack
+   * @param data_on_machine the amount of unique data the task has on the
+   * machine
+   * @return the total task input size
+   */
+  uint64_t ComputeDataStatsForMachine(
+      TaskDescriptor* td_ptr, ResourceID_t machine_res_id,
+      uint64_t* data_on_rack, uint64_t* data_on_machine);
+  Cost_t ComputeTransferCostToMachine(uint64_t remote_data,
+                                      uint64_t data_on_rack);
+  Cost_t ComputeTransferCostToRack(
+      EquivClass_t ec,
+      uint64_t input_size,
+      uint64_t data_on_rack,
+      const unordered_map<ResourceID_t, uint64_t,
+        boost::hash<ResourceID_t>>& data_on_machines);
+  void ConstructTaskPreferredSet(TaskID_t task_id);
+  /**
+   * Get the transfer cost to a resource that is not preferred and on which
+   * the task is currently running.
+   * @param task_id the id of the task
+   * @param res_id the id of the resource on which the task is running
+   */
+  Cost_t GetTransferCostToNotPreferredRes(TaskID_t task_id,
+                                          ResourceID_t res_id);
+  void RemovePreferencesToMachine(ResourceID_t res_id);
+  void RemovePreferencesToRack(EquivClass_t ec);
+  void UpdateMachineBlocks(
+      const DataLocation& location,
+      unordered_map<ResourceID_t, unordered_map<uint64_t, uint64_t>,
+        boost::hash<ResourceID_t>>* data_on_machines);
+  void UpdateRackBlocks(
+      const DataLocation& location,
+      unordered_map<EquivClass_t,
+        unordered_map<uint64_t, uint64_t>>* data_on_racks);
+  /**
+   * Updates a task's cached transfer costs that have been affected by a
+   * change (e.g., machine addition or removal) in a rack.
+   * @param td the descriptor of the task
+   * @param ec_changed the rack in which the machines changed
+   * @param rack_removed true if the rack has already been removed
+   */
+  void UpdateTaskCosts(TaskDescriptor* td_ptr, EquivClass_t ec_changed,
+                       bool rack_removed);
+  Cost_t UpdateTaskCostForRack(TaskDescriptor* td_ptr, EquivClass_t rack_ec);
+
+  /**
+   * Depending on how much data the machine has, the method adds, updates
+   * or removes the machine from the preferred set.
+   * @param input_size the total input of the task
+   * @param machine_res_id the resource descriptor of the machine
+   * @param data_on_machine the amount of task's data (in bytes) the machine
+   * stores
+   * @param transfer_cost the cost to transfer the non-local data
+   * @param task_pref_machines the map storing the task's machine preferences
+   * @return an updated version of the task's machine preference map. If the
+   * passed task_pref_machines is NULL the method may return a new pointer
+   */
+  unordered_map<ResourceID_t, Cost_t, boost::hash<ResourceID_t>>*
+    UpdateTaskPreferredMachineList(
+      TaskID_t task_id,
+      uint64_t input_size,
+      ResourceID_t machine_res_id,
+      uint64_t data_on_machine,
+      Cost_t transfer_cost,
+      unordered_map<ResourceID_t, Cost_t, boost::hash<ResourceID_t>>*
+      task_pref_machines);
+  void UpdateTaskPreferredRacksList(
+      TaskID_t task_id, uint64_t input_size, uint64_t data_on_rack,
+      Cost_t worst_rack_cost, EquivClass_t rack_ec);
+  inline uint64_t GetNumSchedulableSlots(ResourceID_t res_id) {
+    ResourceStatus* rs = FindPtrOrNull(*resource_map_, res_id);
+    CHECK_NOTNULL(rs);
+    if (FLAGS_preemption) {
+      return rs->descriptor().num_slots_below();
+    } else {
+      return rs->descriptor().num_slots_below() -
+        rs->descriptor().num_running_tasks_below();
+    }
+  }
+  inline const TaskDescriptor& GetTask(TaskID_t task_id) {
+    TaskDescriptor* td = FindPtrOrNull(*task_map_, task_id);
+    CHECK_NOTNULL(td);
+    return *td;
+  }
+  inline TaskDescriptor* GetMutableTask(TaskID_t task_id) {
+    TaskDescriptor* td = FindPtrOrNull(*task_map_, task_id);
+    CHECK_NOTNULL(td);
+    return td;
+  }
+
   // Lookup maps for various resources from the scheduler.
   shared_ptr<ResourceMap_t> resource_map_;
   // Information regarding jobs and tasks.
