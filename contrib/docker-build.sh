94754cfc24dc45eab04304218010f101754de4fe
shivramsrivastava <shivram.srivastava@huawei.com>
2018-02-27 13:10:52 +0000

    Modified docker file, updated base image to ubuntu 16.04.
    Affected modules: contrib
    
    Change-Id: I74f3aea47d813064256e3cf7d0fec6faabaf3750

--- contrib/docker-build.sh
+++ contrib/docker-build.sh
@@ -2,4 +2,4 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # Make the docker image and tag it as "firmament"
-docker build -t firmament ${DIR}/docker
+docker build -t firmament ${DIR}/docker --network=host
